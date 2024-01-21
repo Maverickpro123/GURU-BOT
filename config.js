992afe20ab3d3ac434938b7c10a8e61800f82a88
Maverickpro123 <155553040+Maverickpro123@users.noreply.github.com>
2024-01-21 23:32:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['255759662015', 'MAVERICK PRO', true],
+  ['255759662015', 'MAVERICK PRO', true], 
   [''] 
 ] //Number of owners
 
